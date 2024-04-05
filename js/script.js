28ea7a5b60ef7bb0accb64ec64336e80b92fc092
6hislain <ghislainmike80@gmail.com>
2024-04-05 09:29:43 +0200

    typing effect

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
 window.addEventListener("load", function () {
   document.getElementById("loader").style.display = "none";
 });
+
+function type(i, t, ie, oe) {
+  input = document.getElementById(ie).innerHTML;
+  document.getElementById(oe).innerHTML += input.charAt(i);
+  setTimeout(function () {
+    i < input.length - 1 ? type(i + 1, t, ie, oe) : false;
+  }, t);
+}
+
+type(0, 200, "text", "screen");
